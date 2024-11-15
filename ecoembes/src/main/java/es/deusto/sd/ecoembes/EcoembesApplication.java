b715898734500c659a959838a570333b48b2ff5c
contreras-alejandro <alejandro.contreras@opendeusto.es>
2024-11-15 10:48:00 +0100

    Pruebs

--- ecoembes/src/main/java/es/deusto/sd/ecoembes/EcoembesApplication.java
+++ ecoembes/src/main/java/es/deusto/sd/ecoembes/EcoembesApplication.java
@@ -3,6 +3,9 @@ package es.deusto.sd.ecoembes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//prueba
+
 @SpringBootApplication
 public class EcoembesApplication {
 
